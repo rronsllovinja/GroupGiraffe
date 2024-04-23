030645d9cbd21c48972308077a85733cc23680ae
leandrethomas <163021176+leandrethomas@users.noreply.github.com>
2024-04-23 15:33:19 +0100

    Update models.py

--- models.py
+++ models.py
@@ -29,7 +29,32 @@ class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     device = models.ForeignKey(Equipment, on_delete=models.CASCADE)
 
+class Equipment(models.Model):
+    deviceid = models.IntegerField()
+    deviceName = models.CharField(max_length=100)
+    deviceType = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    audit = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, null=True, default=None)
+    comments = models.TextField(blank=True)
 
+    level_categories = [
+        ('technology', 'Technology'),
+        ('gaming', 'Gaming'),
+        ('office', 'Office'),
+        ('other', 'Other'),
+    ]
+    categories = models.CharField(max_length=20, choices=level_categories, default='technology')
+
+    level_availability = [
+        ('available', 'Available'),
+        ('unavailable', 'Unavailable'),
+    ]
+    availability = models.CharField(max_length=20, choices=level_availability, default='available')
+
+    def __str__(self):
+        return self.deviceName
 
 
 

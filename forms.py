299668dba593a40bec069d23a298be13a37408bf
leandrethomas <163021176+leandrethomas@users.noreply.github.com>
2024-04-25 18:29:59 +0100

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,17 +1,7 @@
-# forms.py
 from django import forms
-from .models import Equipment, Rental
+from .models import Equipment
 
 class EquipmentForm(forms.ModelForm):
     class Meta:
         model = Equipment
-        fields = ['name', 'category', 'quantity']
-
-class RentalForm(forms.ModelForm):
-    start_date = forms.DateField(input_formats=['%Y-%m-%d'])
-    end_date = forms.DateField(input_formats=['%Y-%m-%d'])
-    quantity = forms.IntegerField(min_value=1)  # Add this line
-    class Meta:
-        model = Rental
-        fields = ['equipment', 'quantity', 'start_date', 'end_date']  # Include 'quantity'
-
+        fields = '__all__'

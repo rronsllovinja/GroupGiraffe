741a280e2889650201db58478ae0d3f710bfc8a9
w1885108 <w1885108@my.westminster.ac.uk>
2024-04-26 15:01:40 +0100

    message

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_rented_equipment, name='view_rented_equipment'),
     path('eqiupmentlist', views.equipmentlist),
+    path('add_equipment/', views.add_equipment, name = "app/add_equipment.html")
 ]

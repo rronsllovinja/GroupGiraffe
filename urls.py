c7abc800bad8f574e7f544443502b278cce70707
leandrethomas <163021176+leandrethomas@users.noreply.github.com>
2024-05-02 00:53:35 +0100

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_rented_equipment, name='view_rented_equipment'),
-    path('eqiupmentlist', views.equipmentlist),
-    path('add_equipment/', views.add_equipment, name = "app/add_equipment.html")
-]
